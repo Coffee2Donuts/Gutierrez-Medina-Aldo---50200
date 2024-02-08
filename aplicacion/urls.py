fdcf4f4923ec92068183ae68fc81a0cd12092cab
CoffeeDonuts <aldog6851@gmail.com>
2024-02-07 22:42:20 -0600

    Tercera pre-entrega v2.2 - Gutierrez Medina

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('usuario/', usuario, name="usuario"),
+    path('ver_recetas/', ver_recetas, name="ver_recetas"),
+    path('crear_recetas/', crear_recetas, name="crear_recetas"),
+
+    
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "aplicacion/home.html")
+
+def usuario(request):
+    return render(request, "aplicacion/usuario.html")
+
+def ver_recetas(request):
+    return render(request, "aplicacion/ver_recetas.html")
+
+def crear_recetas(request):
+    return render(request, "aplicacion/crear_recetas.html")
